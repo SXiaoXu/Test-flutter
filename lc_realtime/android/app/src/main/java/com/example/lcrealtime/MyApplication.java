d2ec0b62d3d2991ba76b25cd51520e30612e0999
xsui <284898541@qq.com>
2021-01-26 18:57:03 +0800

    add push

--- lc_realtime/android/app/src/main/java/com/example/lcrealtime/MyApplication.java
+++ lc_realtime/android/app/src/main/java/com/example/lcrealtime/MyApplication.java
@@ -1,8 +1,13 @@
 package com.example.lcrealtime;
+import cn.leancloud.AVInstallation;
 import cn.leancloud.AVLogger;
 import cn.leancloud.AVOSCloud;
+import cn.leancloud.AVObject;
 import cn.leancloud.im.AVIMOptions;
+import cn.leancloud.push.PushService;
 import io.flutter.app.FlutterApplication;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 public class MyApplication extends FlutterApplication{
     private static final String LC_App_Id = "WmPQYaUUOkezEpt2RqgH1gyL-gzGzoHsz";
@@ -15,5 +20,25 @@ public class MyApplication extends FlutterApplication{
         AVIMOptions.getGlobalOptions().setUnreadNotificationEnabled(true);
         AVOSCloud.setLogLevel(AVLogger.Level.DEBUG);
         AVOSCloud.initialize(this, LC_App_Id, LC_App_Key, LC_Server_Url);
+        AVInstallation.getCurrentInstallation().saveInBackground().subscribe(new Observer<AVObject>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+            }
+            @Override
+            public void onNext(AVObject avObject) {
+                // 关联 installationId 到用户表等操作。
+                String installationId = AVInstallation.getCurrentInstallation().getInstallationId();
+                System.out.println("保存成功：" + installationId );
+            }
+            @Override
+            public void onError(Throwable e) {
+                System.out.println("保存失败，错误信息：" + e.getMessage());
+            }
+            @Override
+            public void onComplete() {
+            }
+        });
+        PushService.setDefaultPushCallback(this, MainActivity.class);
+        PushService.setDefaultChannelId(this, "channelId");
     }
 }
